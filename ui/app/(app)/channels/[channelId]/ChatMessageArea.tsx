d281d8f5ced9f3304403ef3a117bc40e9407ef58
Michael Toth <mtotho@gmail.com>
2026-04-04 20:11:14 -0400

    Refactor ChatMessageArea component to improve layout by adjusting positioning styles for better responsiveness

--- ui/app/(app)/channels/[channelId]/ChatMessageArea.tsx
+++ ui/app/(app)/channels/[channelId]/ChatMessageArea.tsx
@@ -183,7 +183,7 @@ function WebChatList({
   ) : null;
 
   return (
-    <div style={{ height: "100%", position: "relative", minHeight: 0 }}>
+    <div style={{ position: "absolute", top: 0, left: 0, right: 0, bottom: 0 }}>
       <div
         ref={scrollRef}
         className="chat-scroll-web"
